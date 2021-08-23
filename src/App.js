9d961ef2b2012c4a7b2b6382f183c07e8a9a0f13
AbdelRahmanKhalil <abdelrahmankhalil1@gmail.com>
2021-08-23 11:35:59 +0200

    added new src files

---
+++ src/App.js
@@ -0,0 +1,183 @@
+import React from 'react';
+import './App.css';
+import HeroSection from './components/HeroSection'
+
+
+function App() {
+  return (
+    <>
+    <HeroSection title="THE REVELATION BEGINS" paragraph="Archangel Gabriel meets Prophet Muhammad.
+                    Prophet Muhammad runs to his wife Khadija bint Khwaylid
+                    who introduces him to Waraqa ibn Nawfal, a hanif adhering to the religion of Abraham."  image="/img-0.jpg"/>
+    <HeroSection title="First Islamic Society" paragraph="Prophet Muhammad goes on Hegira (departure from Mecca to Medina). 
+    He's welcomed by the people of Medina. The event marks the birth of the Islamic Society." image="/img-2.jpg"/>
+    <HeroSection title="Rashidun Caliphate Starts" paragraph="Prophet Mohammed dies having conqured Mecca. Most tribes of Arabia are Muslims." image="/img-3.jpg"/>
+    <HeroSection title="Expansion Of The Caliphate"
+    paragraph="Omar ibn Al-Khattab becomes second Caliphate. During his reign, the Caliphate expands from North Africa to Persia."
+    image="/img-4.jpg" />
+    <HeroSection title="Egypt, Land Of Islam"
+    paragraph="Muslim conquest of Egypt is complete after the siege of Alexandria (the capital at the time) ran for months, marking the end of Eastern Roman maritime power. Muslims considered their conquest as the liberation of subjugated people, since in most instances they were under oppressive rule."
+    image="/img-5.jpg" />
+    <HeroSection title="A Decisive Victory In Nahavand"
+    paragraph="Muslim's victory in the Battle of Nahavand laid the groundwork for ending the Sassanid Empire and the conquest of all Persia."
+    image="/img-6.jpg" />
+    <HeroSection title="Civil War Breaks Out"
+    paragraph="Uthman Ibn Affan is assassinated by the Kharajites. The first fitna, civil war, begins between different factions of Muslims."
+    image="/img-7.jpg" />
+    <HeroSection title="Ummayads Rise To Power"
+    paragraph="Muawiyah ibn Abi Sufyan becomes the first Umayyad Caliph after the peace treaty with Hassan ibn Ali, grandson of the Prophet Muhammad and last caliph in Rashidun Caliphate."
+    image="/img-8.jpg" />
+    <HeroSection title="The Battle Of Karbala"
+    paragraph="Husayn ibn Ali, the grandson of Prophet Muhammad, is killed in the battle of Karbala, a day remembered by Shia Muslims to this day."
+    image="/img-9.jpg" />
+    <HeroSection title="The Siege Of Mecca"
+    paragraph="Second fitna, civil war ends, Abd Allah ibn al-Zubayr is martyred after the siege of Mecca."
+    image="/img-10.jpg" />
+    <HeroSection title="Abd Al-Malik Ibn Marwan Creates The Empire"
+    paragraph="Abd al-Malik ibn Marwan makes major reforms in the administrative structure of the Caliphate, including increasing caliphal power, restructuring the army and Arabizing and Islamizing the bureaucracy. He also minted the first Islamic gold Dinar. His reforms laid the groundwork for trade and exchange for centries to come."
+    image="/img-11.jpg" />
+    <HeroSection title="Ahl Al-Ra'y In Iraq"
+    paragraph='The birth of "the great imam" of Sunnis, Abu Hanifa, a theologian and jurist of Persian origin. His school of jurisprudence is the most widely practiced to this day.'
+    image="/img-12.jpg" />
+    <HeroSection title="Poetry Is Truly Arab"
+    paragraph="The rise of poetry under the Umayyad rule. Al-Farazdaq makes it to the personal court of Caliph al-Walid. Jareer and Al-Akhtal's poems survive to this day."
+    image="/img-13.jpg" />
+    <HeroSection title="Islam Reaches Europe"
+    paragraph="Tariq ibn Ziyad disembarks on a journey to cross the Mediterranean Sea, defeats the Visigoths, and start the muslim conquest of the Iberian peninsula."
+    image="/img-14.jpg" />
+    <HeroSection title="The sixth Rashid Caliph"
+    paragraph="Omar ibn Abd al-Aziz rises to power, deemed as the sixth Rashid (rightly-guided) Caliphate. He was a pacifist who cared more spreading Islam than the conquests."
+    image="/img-15.jpg" />
+    <HeroSection title="The Rise Of The Abbasids"
+    paragraph="The Abbasids revolt in Khorsan by virtue of their closer bloodline to Muhammad. The last Ummayad caliph Marwan flees to Egypt, where he was subsequently assassinated. As-Saffah becomes the first Abbasid caliph. The Caliphate moves from Damascus to Baghdad."
+    image="/img-16.jpg" />
+    <HeroSection title="Ummayad Europe Lives On"
+    paragraph='Caliph Al-Mansur, accepting defeat, dubbs Abd al-Rahman I of Andalus "Hawk of Quraysh" and promises never to fight him again. Abbasids would never control Andalus and it would remain under the control of the Ummayads for a few hundered years until its dissolution.'
+    image="/img-17.jpg" />
+    <HeroSection title="Hadith Collection"
+    paragraph="Caliph Al-Mansur requests Imam Malik to produce a standard book that could be promulgated as law in the country. Imam Malik writes his Muwatta, the earliest collection of hadith texts comprising the subjects of Islamic law."
+    image="/img-18.jpg" />
+    <HeroSection title="Harun Al-Rashid and the House Of Wisdom"
+    paragraph="Harun al-Rashid (the rightly guided) comes to power. He creates Bayt Al-Hikmah, House of Wisdom, as a part of the significant Translation Movement taking place during the Abbasid Era, translating works from Greek and Syriac to Arabic."
+    image="/img-19.jpg" />
+    <HeroSection title="The Great Caliph"
+    paragraph="Al-Ma'mun wins the civil war and becomes caliph. A well-educated caliph that sought reconciliation between Greek philosophy and theological questions like the problem of evil and the speech of God, he adopted the Muʿtazila, the rationalist school of theology, that stressed free will and supremacy of human reason over scripture."
+    image="/img-20.jpg" />
+    <HeroSection title="The Mihna Of Qur'an Createdness"
+    paragraph="The caliph al-Ma’mun issued the proclamation of the doctrine of the createdness of the Qur'an. The Mihna, Arabic for tests, begins. Religious persecution lasts for 15 years. Imam Ahmad ibn Hanbal becomes a symbol of the orthodoxy and eventually triumphs."
+    image="/img-21.jpg" />
+    <HeroSection title="The Great Exegesis"
+    paragraph="Al-Tabari is born, an influential Persian scholar, historian and exegete of the Qur'an from Tabaristan (Iran). He writes two of the most influential books in all Islamic History: Tafsir al-Tabari, a Qur'anic commentary, and Tarikh al-Tabari, a historical chronicle."
+    image="/img-22.jpg" />
+    <HeroSection title="Musnad Imam Ahmad"
+    paragraph="Imam Ahmad ibn Hanbal finishes his Musnad, the second great compilation of Hadith in Islamic history at the time."
+    image="/img-23.jpg" />
+    <HeroSection title="The Most Authentic Book Of Hadith"
+    paragraph="Muhammad al-Bukhari finishes his Saheeh, the most authentic book of six most authentic books of Hadith."
+    image="/img-24.jpg" />
+    <HeroSection title="The Second Teacher"
+    paragraph='Al-Farabi is born in Greater Khorasan (modern day Afghanistan). In Islamic philosophical tradition, he is often called "the Second Teacher", Aristotle being the first. His commentaries and treatises on Aristotle and Plato influenced many prominent philosophers like Ibn Sina Avicenna.'
+    image="/img-25.jpg" />
+    <HeroSection title="The Greatest Caliph Of Andalus"
+    paragraph="Abd al-Rahman III rise to power in Andalus. It's said that he was the greatest of the Umayyad rulers of Andalus. He's known for his quote: I have now reigned above fifty years in victory or peace; beloved by my subjects, dreaded by my enemies, and respected by my allies. Riches and honors, power and pleasure, have waited on my call, nor does any earthly blessing appear to have been wanting to my felicity. In this situation, I have diligently numbered the days of pure and genuine happiness which have fallen to my lot."
+    image="/img-26.jpg" />
+    <HeroSection title="The Most Celebrated Of Poets"
+    paragraph="Al-Mutanabbi best Arabic poet of all time is born in Kufa. His most prolific work comes when he's patronised by Sayf al-Dawla Emir of Aleppo."
+    image="/img-27.jpg" />
+    <HeroSection title="A Shia Caliphate In Egypt"
+    paragraph="Ismaili Fatimids conquer Egypt and start a great Caliphate of their own to rival the deteriorating Abbasid Caliphate. At this point, three Caliphates exist, Ummayad in Cordoba, spain, Abbasid in Baghdad, and Fatimids in Cairo."
+    image="/img-28.jpg" />
+    <HeroSection title="The Mad Caliph"
+    paragraph="Al-Hakim bi-Amr Allah, known in the west as the mad caliph, becomes the caliph of the Fatimids. He's the 16th Ismaili imam and an important figure to the Druze (that only lived on in the Levant)."
+    image="/img-29.jpg" />
+    <HeroSection title="Ibn Sina, The Prominant Philosopher"
+    paragraph="Ibn Sina, known in the west as Avicenna, is born in Bukhara. A polymath that got bored of medicine early in his life, he spent his time immersed in philosophy and as all his contemprories proving that philosophy and religion lead to the same conclusions and the ultimate oneness of God. He wrote the Cannon in medicine, the best book in medicine for at least a few hundred years. He contributed to astronomy, alchemy, geography and geology, psychology, Islamic theology, logic, mathematics, physics and works of poetry."
+    image="/img-30.jpg" />
+    <HeroSection title="The Ummayads In Exile Fall"
+    paragraph="Ummayad Caliphate in Andalus falls and disolves into Taifa kingdoms."
+    image="/img-31.jpg" />
+    <HeroSection title="Rise Of Al-Moravids"
+    paragraph="Al-Moravids are the first the Berber Muslim Dynasty in West Africa. They've taken Marrakech as their capital which was founded by their most prominent Sultan Yusuf Ibn Tashfin who at the time got legitimacy by getting the permission of Abbasid Caliph to rule west Africa."
+    image="/img-32.jpg" />
+    <HeroSection title="Al-Ghazali Reviivng The Religion And Attacking Philosophers"
+    paragraph='Al-Ghazali is born in Tus, one of the most prominent thrologians and jurists in the history of Islam. Among many books, two books stand out. In the east, "The Revival of the Religious Sciences" is considered the most notable of his work and is still studied in Halaqs to this day. In the west, "The Incoherence of the Philosophers" is the most well-known book.'
+    image="/img-33.jpg" />
+    <HeroSection title="The Persian Bureaucrat"
+    paragraph="Nizam al-Mulk becomes vizier of the Seljuk Empire, he was the first person to start the madrasa system called Nizamiyeh, first well organized institutions of higher learning in the Muslim world, at the time. Nizam al-Mulk asks Al-Ghazali to lead it."
+    image="/img-34.jpg" />
+    <HeroSection title="A Muslim Anatolia"
+    paragraph="A decisive win for the Suljuks under Alp Arselan over the Byzantines in the battle of Manzikert is still celebrated to this day in Turkey."
+    image="/img-35.jpg" />
+    <HeroSection title="The Rise of Al-Mohad Caliphate"
+    paragraph="The Almohad movement was founded by Ibn Tumart among the Berber Masmuda tribes in the south of modern Morocco. Around 1120, the Almohads first established a Berber state in Tinmel in the Atlas Mountains."
+    image="/img-36.jfif" />
+    <HeroSection title="Ibn Rushd, The Polymath"
+    paragraph='Ibn Rushd (Averroes) is born in Cordoba. A polymath and jurist who wrote about many subjects, including philosophy, medicine, astronomy. A strong proponent of Aristotelianism. Ibn Rushd defends philosophers and writes his infamous "Incoherence of the Incoherence" to reply to Al-Ghazalis arguments.'
+    image="/img-37.jpg" />
+    <HeroSection title="Salah ad-Din"
+    paragraph="Salah ad-Din rises through the ranks to become sunni vizier to the caliph of Fatimids abolishes the Ismaili Caliphate and brings Egypt's rule under the Abbasid Caliphate once age. He founds the Ayyubid dynasty which stretches from Iraq to Morocco."
+    image="/img-38.jpg" />
+    <HeroSection title="The Battle Of Hattin"
+    paragraph="The Muslim armies under Saladin free Jerusalem from the crusaders and bring the Holy Land under the Muslim control once again."
+    image="/img-39.jpg" />
+    <HeroSection title="Sultana Shajar Al-Durr"
+    paragraph="As-Salih dies during the seventh crusades. His wife Shajar al-Durr becomes Sultana. She then marries Aybek whom she kills. Later on, she's beaten to death with clogs by the bondmaids of Om Ali."
+    image="/img-40.jpg" />
+    <HeroSection title="Mamluks Sultanate"
+    paragraph="Shajar al-Durr with her second husband Aybek create the Mamluks Sultanate. A sultanate made up of slave warriors who become Sultans based on their merit and bringing the factions together rather than bloodline. The Sultanate is so effective that it destroys the crusades and the mongols once and for all."
+    image="/img-41.jpg" />
+    <HeroSection title="The Siege of Baghdad"
+    paragraph="(..)https://en.wikipedia.org/wiki/Siegeof_Baghdad(1258)"
+    image="/img-42.jpg" />
+    <HeroSection title="Ain Jalut"
+    paragraph="Qutuz, mamluk Sultan of Egypt, destroys the mongol army in the battle of Ain Jalut in Palestine."
+    image="/img-43.jpg" />
+    <HeroSection title="Baibars"
+    paragraph="The Greatest Sultan Baibars rise to power."
+    image="/img-44.jfif" />
+    <HeroSection title="Shaykh al-Islam Ibn Taymiyyah"
+    paragraph="Ibn Taymiyyah is probably the most influential scholar of Islam since Al-Ghazali. A Hanbali jurist, muhaddith, and theologian, he's considered a mujaddid. He participated in rallying the troops in the battles against the mongols and was imprisoned seven times for contraversial fatwas. He wrote treaties on almost every Islamic science from the attributes of Allah to 'Averting the Conflict between Reason and (religious) Tradition' ."
+    image="/img-45.jpg" />
+    <HeroSection title="Richest Man Ever"
+    paragraph="Mansy Musa"
+    image="/img-46.jpg" />
+    <HeroSection title="Ibn Battuta Returns"
+    paragraph="Ibn Battuta returns to Morocco."
+    image="/img-47.jpg" />
+    <HeroSection title="Ibn Khaldun"
+    paragraph="The first sociologist and modern historian Ibn Khaldun"
+    image="/img-48.jpg" />
+    <HeroSection title="The Prophet's Prophecy"
+    paragraph="The Conquest Of Constantinople"
+    image="/img-49.jpg" />
+    <HeroSection title="The Admiral Of The Mediterranean"
+    paragraph="Khayreddin Barbarossa..."
+    image="/img-50.jpg" />
+    <HeroSection title="A Shia Dynasty In Persia"
+    paragraph="The Safavids rise to power in Persia."
+    image="/img-51.svg" />
+    <HeroSection title="The End Of Mamluks"
+    paragraph="The last Mamluk Sultan Tuman bay II was cast into prison, and shortly after, hanged as a malefactor at the City Gate."
+    image="/img-52.jpg" />
+    <HeroSection title="Mughal Empire"
+    paragraph="The Mughul Empire"
+    image="/img-53.jpeg" />
+    <HeroSection title="Napoleon In The Middle East"
+    paragraph="The French Campaign"
+    image="/img-54.jpg" />
+    <HeroSection title="Slicing The Cake"
+    paragraph="Sykes-Picot"
+    image="/img-55.jpg" />
+    <HeroSection title="Atatürk Abolishes The Caliphate"
+    paragraph="The defeat of the Germans and the Ottomans in WWI had severe repurcussions..."
+    image="/img-56.jpg" />
+    <HeroSection title="The Rise Of Nationalism"
+    paragraph="Gamal Abd el-Naser, et al. Nationalism gained grounds during the 20th century in the orient."
+    image="/img-57.jpg" />
+    
+
+</>
+  );
+}
+
+export default App;
